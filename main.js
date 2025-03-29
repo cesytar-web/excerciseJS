7f02476d37cb014ffdc764a50e7f7e8b01cc9981
cesytar-web <cesytarr@gmail.com>
2025-03-29 23:40:07 +0100

    Prueba JavaS1

---
+++ main.js
@@ -0,0 +1,9 @@
+const name = "cecilia"
+
+console.log(name.toUpperCase())
+console.log(name.split(''))
+
+function contarPalabras(name1) {
+    return name1.split(" ").length;
+}
+console.log(contarPalabras("Jose Maria Jesus"));
